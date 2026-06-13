6f95b9ba8dff13121c439151f142c29e6fd26c35
trankhiem0210 <khiema5436@gmail.com>
2026-06-13 07:30:26 +0700

    commit code

--- src/model/ParkingSlot.java
+++ src/model/ParkingSlot.java
@@ -3,9 +3,43 @@
  */
 package model;
 
+import enums.VehicleType;
+
 /**
  * 
  */
 public class ParkingSlot {
-
+	private String slotId;
+	private VehicleType vehicleType;
+	private boolean isOccupied;
+	/**
+	 * @param slotId
+	 * @param vehicleType
+	 * @param isOccupied
+	 */
+	public ParkingSlot(String slotId, VehicleType vehicleType, boolean isOccupied) {
+		super();
+		this.slotId = slotId;
+		this.vehicleType = vehicleType;
+		this.isOccupied = isOccupied;
+	}
+	public String getSlotId() {
+		return slotId;
+	}
+	public void setSlotId(String slotId) {
+		this.slotId = slotId;
+	}
+	public VehicleType getVehicleType() {
+		return vehicleType;
+	}
+	public void setVehicleType(VehicleType vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+	public boolean isOccupied() {
+		return isOccupied;
+	}
+	public void setOccupied(boolean isOccupied) {
+		this.isOccupied = isOccupied;
+	}
+	
 }
--- src/repository/DataStorage.java
+++ src/repository/DataStorage.java
@@ -1,10 +1,14 @@
 package repository;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import enums.Role;
+import enums.VehicleType;
+import model.ParkingArea;
 import model.ParkingSlot;
+import model.ParkingTicket;
 import model.SubscriptionCard;
 import model.User;
 
@@ -14,21 +18,41 @@ public class DataStorage {
     private List<User> users;
     private List<ParkingSlot> parkingSlots;
     private List<SubscriptionCard> subscriptionCards;
+    private List<ParkingTicket> parkingTickets;
+    private List<ParkingTicket> parkingTicketsHistory;
+    private List<ParkingArea> parkingAreas;
 
     // Sử dụng private constructor để áp dụng Singleton Pattern
     private DataStorage() {
         this.users = new ArrayList<>();
         this.parkingSlots = new ArrayList<ParkingSlot>();
         this.subscriptionCards = new ArrayList<SubscriptionCard>();
-        
-        initializeMockData();
+        this.parkingTickets = new ArrayList<ParkingTicket>();
+        this.parkingTicketsHistory = new ArrayList<ParkingTicket>();
+        this.parkingAreas = new ArrayList<ParkingArea>();
+
+        initDefaultData();
     }
 
-    private void initializeMockData() {
+    private void initDefaultData() {
         // Thêm một số người dùng mẫu
         users.add(new User("admin", "admin123", Role.ADMIN));
         users.add(new User("user1", "password1", Role.STAFF));
         users.add(new User("user2", "password2", Role.STAFF));
+        
+        subscriptionCards.add(new SubscriptionCard("C-001", "ABC-123", "Khách Hàng A", VehicleType.CAR, LocalDate.now().plusMonths(1))); 
+		subscriptionCards.add(new SubscriptionCard("C-002", "DEF-456", "Khách Hàng B", VehicleType.CAR, LocalDate.now().minusDays(5)));
+   
+		parkingAreas.add(new ParkingArea("A", "Khu A", VehicleType.CAR));
+		parkingAreas.add(new ParkingArea("B", "Khu B", VehicleType.MOTORBIKE));
+		
+		for (int i = 1; i < 10; i++) {
+			parkingSlots.add(new ParkingSlot("A" + i, VehicleType.CAR, false));
+		}
+		for (int i = 1; i < 10; i++) {
+			parkingSlots.add(new ParkingSlot("B-"+ i, VehicleType.MOTORBIKE, false));
+		}
+		
     }
 
     // Hàm lấy instance duy nhất của DataStorage
